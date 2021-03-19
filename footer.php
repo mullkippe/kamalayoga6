07939c14485f0840022bc649501aafffce8532ea
AYUMI WADA <65645313+mullkippe@users.noreply.github.com>
2021-03-19 13:52:59 +0900

    ヘッダー、フッター、ロゴマークのファイル

---
+++ footer.php
@@ -0,0 +1,42 @@
+  <footer class="footer">
+        <div class="footer-container">
+            <ul class="footer-contents">
+                <li class="footer-title"><a href="./"><span class="footer-top">HOME</span></a></li>
+                <li class="footer-title"><a href="about.php"><span>かまらヨガについて</span></a>
+                    <ul class="content-list">
+                        <li><a href="about.php#link-one">▶かまらヨガとは</a></li>
+                        <li><a href="about.php#link-two">▶代表・スタッフ</a></li>
+                        <li><a href="about.php#link-three">▶かまらヨガの特長</a></li>
+                    </ul>
+
+                </li>
+                <li class="footer-title"><a href="#service"><span>私たちの取り組み</span></a>
+                    <ul class="content-list">
+                        <li><a href="retreat.php">▶ヨガリトリート</a></li>
+                        <li><a href="study.php">▶ヴェーダーンタ勉強会</a></li>
+                        <li><a href="maternity.php">▶助産院プラサーダ</a></li>
+                        <li><a href="stressmanagement.php">▶保健室オンライン</a></li>
+                    </ul>
+                </li>
+                <li class="footer-title left"><a href="https://coubic.com/scentofbliss/services"><span>ご予約</span></a></li>
+                <li class="footer-title left"><a href="access.php"><span>アクセス</span></a></li>
+                <li class="footer-title left"><a href="question.php"><span>Q＆A</span></a>
+                    <ul class="right-title left">
+                        <li><a href="news.php"><span>NEWS</span></a></li>
+                        <li><a href="blogtop.php"><span>BLOG</span></a></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <div class="messagebtn">
+                <a href="contact.php" class="btn"><span class="innerbtn">お問い合わせ　→</span></a>
+
+            </div>
+            <h1 class="footer-logo"><a href="./"><img src="images/toppage/minilogo.png" alt="ロゴマーク"></a></h1>
+            <div class="sns">
+                <a href="https://www.facebook.com/kaorikamalayoga/"><img src="images/toppage/minifb.png" alt=""></a>
+                <a href="#"><img src="images/toppage/miniinstagram.png" alt=""></a>
+                <a href="#"><img src="images/toppage/minitwitter.png" alt=""></a>
+            </div>
+        </div>
+    </footer>
---
+++ header.php
@@ -0,0 +1,34 @@
+<header role="banner">
+        <button type="button" class="drawer-toggle drawer-hamburger">
+            <span class="sr-only">toggle navigation</span>
+            <span class="drawer-hamburger-icon"></span>
+        </button>
+
+        <h1><a href="./"><img src="images/toppage/minilogo.png" alt="ロゴマーク"></a></h1>
+
+        <nav class="header-list drawer-nav" role="navigation">
+            <ul class="drawer-menu">
+                <li class="menu-item-has-children">
+                    <a href="about.php" class="drawer-brand">かまらヨガについて</a>
+                    <ul class="sub-menu">
+                        <li><a href="about.php#link-one" class="drawer-menu-item">かまらヨガとは</a></li>
+                        <li><a href="about.php#link-two" class="drawer-menu-item">代表・スタッフ紹介</a></li>
+                        <li><a href="about.php#link-three" class="drawer-menu-item">かまらヨガのこだわり</a></li>
+                    </ul>
+                </li>
+                <li class="two menu-item-has-children">
+                    <a href="retreat.php" class="drawer-brand">私たちの取り組み</a>
+                    <ul class="sub-menu">
+                        <li><a href="retreat.php" class="drawer-menu-item">ヨガ・リトリート</a></li>
+                        <li><a href="study.php" class="drawer-menu-item">ヴェーダーンタ勉強会</a></li>
+                        <li><a href="maternity.php" class="drawer-menu-item">助産院プラサーダ</a></li>
+                        <li><a href="stressmanagement.php" class="drawer-menu-item">保健室オンライン</a></li>
+                    </ul>
+                </li>
+                <li class="three"><a href="https://coubic.com/scentofbliss/services" class="drawer-brand">ご予約</a></li>
+                <li><a href="access.php" class="drawer-brand">アクセス</a></li>
+                <li><a href="voice.php" class="drawer-brand">お客様の声</a></li>
+            </ul>
+        </nav>
+
+    </header>
